4642cd41b8922066ffb407bd32d82ea9b2a0eb4c
rybaier <rybaier@gmail.com>
2022-03-09 10:19:20 -0800

    user model created

---
+++ models/User.js
@@ -0,0 +1,65 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+const UserSchema = new mongoose.Schema(
+  {
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      password: {
+        type: String,
+        required: true,
+      },
+    },
+    {
+      timestamps: true,
+      toJSON: {
+        virtuals: true,
+        transform: (_doc, ret) => {
+          delete ret.password;
+          return ret;
+        },
+      },
+    }
+  );
+// use function keyword instead of arrow function to gain access to entire this instance of file
+//before saving user to db salt and hash password
+UserSchema.pre('save', function(next){
+    const user =this
+    if(!user.isModified('password')){
+        return next()
+    }
+    bcrypt.genSalt(10, (err, salt) =>{
+        if (err) {
+            return next(err)
+        }
+    
+        bcrypt.hash(user.password, salt, (err, hash) => {
+            if (err){
+                return next(err)
+            }
+            user.password = hash
+            next()
+        })
+    })
+})
+//compare passswords for authentication use
+UserSchema.methods.comparePassword = function(candidatePasswork) {
+    const user = this
+    return new Promise((resolve, reject) => {
+        bcrypt.compare(comparePassword, user.password, (err, isMatch) => {
+            if (err) {
+                return reject(err)
+            }
+            if(!isMatch){
+                return reject(false)
+            }
+            resolve(true)
+        })
+    })
+}
+
+const User = mongoose.model('User', UserSchema)
+module.exports = User
